27ffd83166eefebdce7c0bded5020ac9ac83af14
zzzzzzzxh <774665106@qq.com>
2026-01-15 14:54:44 +0800

    add doc for eagle3

--- examples/run_qwen_eagle3.py
+++ examples/run_qwen_eagle3.py
@@ -1,14 +1,9 @@
-import os
-
 import sglang as sgl
-
-os.environ["SGLANG_ENABLE_SPEC_V2"] = "1"
-os.environ["ASCEND_LAUNCH_BLOCKING"] = "1"
 
 
 def main():
     llm = sgl.Engine(
-        model_path="/home/wangtiance/Qwen3-8B",
+        model_path="/home/ckpt/Qwen3-8B",
         device="npu",
         model_impl="mindspore",
         attention_backend="ascend",
@@ -18,21 +13,18 @@ def main():
         mem_fraction_static=0.75,
         port=24678,
         disable_cuda_graph=True,
-        speculative_draft_model_path="/home/wangtiance/Qwen3-8B_eagle3",
+        speculative_draft_model_path="/home/ckpt/Qwen3-8B_eagle3",
         speculative_algorithm="EAGLE3",
         speculative_num_steps=4,
         speculative_eagle_topk=1,
-        speculative_num_draft_tokens=16,
-        context_length=8192,
+        speculative_num_draft_tokens=5,
     )
-    os.environ["SGL_TOKENIZER_PATH"] = "/home/wangtiance/Qwen3-8B"
 
     prompts = [
         "what is mindspore?",
-        "what is mindspore?",
     ]
 
-    sampling_params = {"temperature": 0, "top_p": 1.0, "max_new_tokens": 5}
+    sampling_params = {"temperature": 0, "top_p": 1.0}
 
     outputs = llm.generate(prompts, sampling_params)
     for prompt, output in zip(prompts, outputs):
